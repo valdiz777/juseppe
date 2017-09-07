c239a11ed6697050e60ad767b00b696dca6f44e8
Valentine Nwachukwu <valentine.nwachukwu@ngc.com>
2017-09-07 00:12:07 -0500

    fixing formating issues

--- juseppe-core/src/main/java/ru/lanwen/jenkins/juseppe/gen/source/PathPluginSource.java
+++ juseppe-core/src/main/java/ru/lanwen/jenkins/juseppe/gen/source/PathPluginSource.java
@@ -6,12 +6,14 @@ import ru.lanwen.jenkins.juseppe.beans.Plugin;
 import ru.lanwen.jenkins.juseppe.gen.HPI;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.lang.String.format;
 
@@ -20,32 +22,33 @@ import static java.lang.String.format;
  */
 public class PathPluginSource implements PluginSource {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PathPluginSource.class);
-    private final Path pluginsDir;
-    private final boolean recursiveWatch;
+	private static final Logger LOG = LoggerFactory.getLogger(PathPluginSource.class);
+	private final Path pluginsDir;
+	private final boolean recursiveWatch;
 
-    public PathPluginSource(Path pluginsDir, boolean recursiveWatch) {
-        this.pluginsDir = pluginsDir;
-        this.recursiveWatch = recursiveWatch;
-    }
+	public PathPluginSource(Path pluginsDir, boolean recursiveWatch) {
+		this.pluginsDir = pluginsDir;
+		this.recursiveWatch = recursiveWatch;
+	}
 
-    @Override
-    public List<Plugin> plugins() {
-		  try (Stream<Path> paths = (recursiveWatch)? Files.walk(pluginsDir): Files.list(pluginsDir)) {
-		  	   return paths.filter(path -> path.toString().endsWith(".hpi") || path.toString().endsWith(".jpi")).map(path -> {
-					 try {
-					 	  LOG.trace("Process file {}", path);
+	@Override
+	public List<Plugin> plugins() {
+		try (Stream<Path> paths = (recursiveWatch) ? Files.walk(pluginsDir) : Files.list(pluginsDir)) {
+			return paths.filter(path -> path.toString().endsWith(".hpi") || path.toString().endsWith(".jpi"))
+				.map(path -> {
+				try {
+					LOG.trace("Process file {}", path);
 
-						  return HPI.loadHPI(path.toFile())
-									 .withUrl(pluginsDir.relativize(path).toString());
+					return HPI.loadHPI(path.toFile())
+							.withUrl(pluginsDir.relativize(path).toString());
 
-					 } catch (Exception e) {
-						  LOG.error("Fail to get the {} info", path.toAbsolutePath(), e);
-						  return null;
-					 }
-				}).filter(Objects::nonNull).collect(Collectors.toList());
-		  } catch (IOException e) {
-			   throw new RuntimeException(format("Can't read path %s", pluginsDir.toAbsolutePath()), e);
-		  }
-	 }
+				} catch (Exception e) {
+					LOG.error("Fail to get the {} info", path.toAbsolutePath(), e);
+					return null;
+				}
+			}).filter(Objects::nonNull).collect(Collectors.toList());
+		} catch (IOException e) {
+			throw new RuntimeException(format("Can't read path %s", pluginsDir.toAbsolutePath()), e);
+		}
+	}
 }
